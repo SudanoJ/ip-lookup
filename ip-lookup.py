8af18a68ad68ebb18feefcee4e005b2fb6a69ab1
Sudano <40310686+SudanoJ@users.noreply.github.com>
2021-01-18 03:54:08 -0300

    Add files via upload

---
+++ ip-lookup.py
@@ -0,0 +1,29 @@
+import requests
+import json
+
+logo=r"""
+ _             _             _                
+(_)           | |           | |               
+ _ _ __ ______| | ___   ___ | | ___   _ _ __  
+| | '_ \______| |/ _ \ / _ \| |/ / | | | '_ \ 
+| | |_) |     | | (_) | (_) |   <| |_| | |_) |
+|_| .__/      |_|\___/ \___/|_|\_\\__,_| .__/ 
+  | |                                  | |    
+  |_|                                  |_|    
+                        by Sudano#7479
+"""
+print(logo)
+
+ip = input("IP: ")
+
+r = requests.get('http://extreme-ip-lookup.com/json/' + ip)
+y = json.loads(r.text)
+
+if y["status"] in "fail":
+  print("Ocorreu um erro ao pegar as informações do IP")
+  exit()
+
+print("Cidade: " + y["city"])
+print("Estado: " + y["region"])
+print("País: " + y["country"])
+print("Lat: " + y["lat"] + " | Lon: " + y["lon"])
